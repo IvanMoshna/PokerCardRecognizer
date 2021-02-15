75f73da504a65fc0dfaf94b319b0b304bd2b50f4
Ivan Moshna <ittifock@gmail.com>
2021-02-15 14:48:54 +0700

    creates paths generators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,69 @@
 package com.company;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        final String path = "G:/JavaProjects/cardsExample/src/SampleImages";
+
+        //путь к шаблонам
+        //TODO:разделить масти и ранги по папкам
+        Path resourcesPath = Paths.get("resources");
+        String absoluteResourcesPath = resourcesPath.toFile().getAbsolutePath();
+        String normalResourcesPath = absoluteResourcesPath.replace("\\", "/");
+        String normalSoughtPath = absoluteResourcesPath.replace("\\", "/");
+
+        List<Path> resourcesPathList = getImgPathNames(normalResourcesPath);
+        List<Path> pathList = getImgPathNames(normalSoughtPath);
+
+        List<BufferedImage> bufferedImages = getImageListFromPaths(pathList);
+
+
+
+
+    }
+
+    public static List<Path> getImgPathNames(String path) {
+       /* File dir = new File(path);
+        List<String> imgNames = new ArrayList<>();
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                imgNames.add(item.getName());
+            }
+        }*/
+        List<Path> pathToFileList = new ArrayList<>();
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            paths.filter(Files::isRegularFile).forEach(pathToFileList::add);
+        } catch (IOException e) {
+            System.err.println("Wrong path or incorrect access : ");
+            e.printStackTrace();
+        }
+        return pathToFileList;
+    }
+
+    public static List<BufferedImage> getImageListFromPaths(List<Path> pathList) {
+        List<BufferedImage> resultList = new ArrayList<>();
+        for (Path p:pathList) {
+            BufferedImage bufferedImage = null;
+            try {
+                bufferedImage = ImageIO.read(p.toFile());
+                resultList.add(bufferedImage);
+            } catch (IOException e) {
+                System.err.println("Cannot read the image file : ");
+                e.printStackTrace();
+            }
+        }
+        return resultList;
     }
 }
