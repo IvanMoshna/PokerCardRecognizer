4055e6c24f0117f365f2c877e4570fcba7b7a00b
Ivan Moshna <ittifock@gmail.com>
2021-02-15 16:50:42 +0700

    add constants

--- src/com/company/Constants.java
+++ src/com/company/Constants.java
@@ -1,6 +1,8 @@
 package com.company;
 
 public class Constants {
-
-
+    public static final int[] CARDS_POS_X = {153, 225, 296, 368, 439};
+    public static final int RANK_POS_Y = 592;
+    public static final int SUIT_POS_Y = 634;
+    public static final int SUIT_OFFSET_X = 28;
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -54,6 +56,17 @@ public class Main {
             e.printStackTrace();
         }
         return pathToFileList;
+    }
+
+    public static BufferedImage readImageFile(Path pathToFile) {
+        BufferedImage imageSource = null;
+        try {
+            imageSource = ImageIO.read(pathToFile.toFile());
+        } catch (IOException e) {
+            System.err.println("Cannot read the image file : ");
+            e.printStackTrace();
+        }
+        return imageSource;
     }
 
     public static List<BufferedImage> getImageListFromPaths(List<Path> pathList) {
@@ -102,4 +115,126 @@ public class Main {
         }
         return resultList;
     }
+    //TODO:объеденить методы
+    private static StringBuilder getStringFromImage(File file) throws IOException {
+        BufferedImage image = ImageIO.read(file);
+        BufferedImage symbol = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        Graphics2D g = symbol.createGraphics();
+        g.drawImage(image,0,0, null);
+
+
+        int height = image.getHeight();
+        int width = image.getWidth();
+        short whiteBg = -1;
+        StringBuilder binaryString = new StringBuilder();
+        for (short y = 1; y < height; y++)
+            for (short x = 1; x < width; x++) {
+                int rgb = symbol.getRGB(x, y);
+                binaryString.append(rgb == whiteBg ? " " : "*");
+            }
+        return binaryString;
+    }
+
+    public static void levenshteinCompareMethod(Map<String, Path> templateImageMap,
+                                          List<BufferedImage> sampleImageList) throws IOException {
+        for (int i = 0; i < sampleImageList.size(); i++) {
+            BufferedImage identifyImage = sampleImageList.get(i);
+            String[] foundRank = {"?", "?", "?", "?", "?"};
+            String[] foundSuits = {"?", "?", "?", "?", "?"};
+
+            for (int cardNum = 0; cardNum < Constants.CARDS_POS_X.length; cardNum++) {
+                //обрабатываем ранг
+                int min = Integer.MAX_VALUE;
+                for (Map.Entry<String, Path> entry : templateImageMap.entrySet()) {
+                    String rankName = entry.getKey();
+                    Path templatePath = entry.getValue();
+                    File templateFile = new File(String.valueOf(templatePath));
+                    BufferedImage templateImage = readImageFile(templatePath);
+
+                    int offset = 11;
+
+                    //TODO:ОПТИМИЗАЦИЯ
+                    File cutFileImage = getSubFileFromImage(identifyImage, templateImage,
+                            Constants.CARDS_POS_X[cardNum] + offset,
+                            Constants.RANK_POS_Y);
+                    StringBuilder templateStringFile = getStringFromImage(templateFile);
+                    StringBuilder cutStringFile = getStringFromImage(cutFileImage);
+                    int levenshtein = levensteinMin(cutStringFile.toString(), templateStringFile.toString());
+                    if (levenshtein <= min && levenshtein <=60) {
+                        min = levenshtein;
+                        foundRank[cardNum] = rankName;
+                    }
+
+                }
+                //TODO: МАСТИ
+                for (Map.Entry<String, Path> entry : templateImageMap.entrySet()) {
+
+                    String suitName = entry.getKey();
+                    Path templatePath = entry.getValue();
+                    File templateFile = new File(String.valueOf(templatePath));
+                    BufferedImage templateImage = readImageFile(templatePath);
+
+                    File cutFileImage = getSubFileFromImage(identifyImage, templateImage,
+                            Constants.CARDS_POS_X[cardNum]  + Constants.SUIT_OFFSET_X,
+                            Constants.SUIT_POS_Y);
+
+                    StringBuilder templateStringFile = getStringFromImage(templateFile);
+                    StringBuilder cutStringFile = getStringFromImage(cutFileImage);
+                    int levenshtein = levensteinMin(cutStringFile.toString(), templateStringFile.toString());
+                    min = 30;
+                    if (levenshtein <= min) {
+                        foundSuits[cardNum] = suitName;
+                    }
+                }
+
+            }
+            //TODO:сделать нормальный вывод
+
+            for(int j = 0; j<foundRank.length; j++)
+            {
+                System.out.print(foundRank[j]+""+foundSuits[j]);
+            }
+            System.out.println();
+        }
+    }
+    public static int levensteinMin( String str1, String str2) {
+        int[] Di_1 = new int[str2.length() + 1];
+        int[] Di = new int[str2.length() + 1];
+
+        for (int j = 0; j <= str2.length(); j++) {
+            Di[j] = j; // (i == 0)
+        }
+
+        for (int i = 1; i <= str1.length(); i++) {
+            System.arraycopy(Di, 0, Di_1, 0, Di_1.length);
+
+            Di[0] = i; // (j == 0)
+            for (int j = 1; j <= str2.length(); j++) {
+                int cost = (str1.charAt(i - 1) != str2.charAt(j - 1)) ? 1 : 0;
+                Di[j] = min(
+                        Di_1[j] + 1,
+                        Di[j - 1] + 1,
+                        Di_1[j - 1] + cost
+                );
+            }
+        }
+        return Di[Di.length - 1];
+    }
+
+    private static int min(int n1, int n2, int n3) {
+        return Math.min(Math.min(n1, n2), n3);
+    }
+
+    public static File getSubFileFromImage(BufferedImage identifyImage,
+                                           BufferedImage templateImage, int offsetX, int positionY) throws IOException {
+        BufferedImage carvedImage = identifyImage.getSubimage(offsetX, positionY,
+                templateImage.getWidth(), templateImage.getHeight());
+
+        carvedImage = ImageService.getBinarizedImage(carvedImage);
+
+        File outputStream = new File("compare.png");
+        ImageIO.write(carvedImage, "png", outputStream);
+        return outputStream;
+    }
+
 }
